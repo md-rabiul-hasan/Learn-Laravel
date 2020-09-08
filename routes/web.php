efcb429f605d33c4b2b85d02f6431de24c4455c3
md-rabiul-hasan <mdrabiulhasan.me@gmail.com>
2020-09-09 01:06:56 +0600

    Basic Routing

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,35 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/home',function(){
+    return "welcome home";
+});
+
+Route::match(['get','post'],'/get',function(){
+    return "welcome get request";
+});
+
+Route::any('/any',function(){
+    return "welcome to any request";
+});
+
+
+// Route Parameter
+Route::get('/home/{name}',function($name){
+    return "Hi {$name}";
+});
+
+Route::get('/name/{name?}',function($name = null){
+    if(!empty($name)){
+        echo "Hey Mr. {$name}";
+    }else{
+        echo "Please Enter Your Name";
+    }
+});
+
+// url validation with regex
+Route::get('book/{id}',function($id){
+    return "Detial of book id => {$id}";
+})->where('id','[0-9]+');
